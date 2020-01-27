8228a22b7035339386b50ab477afc8607e755649
dernathul <lautaroparracab@gmail.com>
2020-01-27 15:46:37 +0100

    calculator with button

--- cypress/integration/userCanEnterWeightAndHeight.feature.js
+++ cypress/integration/userCanEnterWeightAndHeight.feature.js
@@ -10,3 +10,13 @@ describe('User can check their Bmi', () => {
         cy.contains('BMI Calculator')
     })
 })
+
+describe('User can check their Bmi', () => {
+    it('user can enter weight and height', () => {
+        cy.contains('BMI Calculator')
+        cy.get('#weight').type('90')
+        cy.get('#height').type('186')
+        cy.get('#Calculate').click()
+
+    })
+})
