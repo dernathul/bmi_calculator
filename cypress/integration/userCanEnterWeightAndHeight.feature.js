732fe353f2285d3a018e125af10ea81d4942138d
dernathul <lautaroparracab@gmail.com>
2020-01-28 09:24:41 +0100

    bmi calkculator almost finished

--- cypress/integration/userCanEnterWeightAndHeight.feature.js
+++ cypress/integration/userCanEnterWeightAndHeight.feature.js
@@ -17,6 +17,10 @@ describe('User can check their Bmi', () => {
         cy.get('#weight').type('90')
         cy.get('#height').type('186')
         cy.get('#Calculate').click()
-
     })
+
+    it('displays a BMI value of 26.01', () => {
+        cy.get('#results').should('contain', '26.01')
+    })
+
 })
